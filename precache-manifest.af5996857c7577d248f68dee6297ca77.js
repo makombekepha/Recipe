6fa858f8ae2e84b7ebda155d554745158d3d61fa
makombekepha <makombekepha@gmail.com>
2020-05-10 13:11:57 +0300

    Updates

---
+++ precache-manifest.af5996857c7577d248f68dee6297ca77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa4feadd651c69683edc75e92d85ab7",
+    "url": "/Recipe.git/index.html"
+  },
+  {
+    "revision": "06e7825df95b299d3679",
+    "url": "/Recipe.git/static/css/main.02fd8937.chunk.css"
+  },
+  {
+    "revision": "710eced775d89082d9c6",
+    "url": "/Recipe.git/static/js/2.8093d9b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe.git/static/js/2.8093d9b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e7825df95b299d3679",
+    "url": "/Recipe.git/static/js/main.e90296de.chunk.js"
+  },
+  {
+    "revision": "c59a7b08eebedb712566",
+    "url": "/Recipe.git/static/js/runtime-main.fc6da122.js"
+  }
+]);
